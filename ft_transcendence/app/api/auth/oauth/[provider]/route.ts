81a313b1bace8008682c15a12f189730ccf4409c
pjerddee <penfah.j@gmail.com>
2025-12-26 00:13:23 +0700

    add [provider]/route.ts

--- ft_transcendence/app/api/auth/oauth/[provider]/route.ts
+++ ft_transcendence/app/api/auth/oauth/[provider]/route.ts
@@ -10,10 +10,25 @@ const PROVIDER_TO_EMAIL: Record<string, string> = {
 };
 
 export async function GET(
-  _req: Request,
-  { params }: { params: { provider: string } }
+  req: Request,
+  { params }: { params: Promise<{ provider: string }> }
 ) {
-  const provider = params.provider;
+  const { provider } = await params;
+  if (provider === 'google') {
+    const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+    const options = {
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI!,
+      client_id: process.env.GOOGLE_CLIENT_ID!,
+      access_type: "offline",
+      response_type: "code",
+      prompt: "consent",
+      scope: "openid email profile",
+    };
+
+    const qs = new URLSearchParams(options);
+    return NextResponse.redirect(`${rootUrl}?${qs.toString()}`);
+  }
+
   const email = PROVIDER_TO_EMAIL[provider];
 
   if (!email) {
